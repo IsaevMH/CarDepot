56bc30a9d4d772e4f6fdf8c4ad10b35b4a058161
Maxim Isaev <iwork30rus@gmail.com>
2021-06-16 11:08:30 +0400

    1. Added methods to work with objects in MainApp

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -24,7 +24,6 @@ public class MainApp {
         int option = in.nextInt();
         return option;
     }
-
     public static int displayCarsInfo(){
         System.out.println("----------------Автомобиль------------------");
         System.out.println("1. Добавить");
@@ -35,7 +34,6 @@ public class MainApp {
         int option = in.nextInt();
         return option;
     }
-
     public static int displayDriversInfo() {
         System.out.println("----------------Водитель--------------------");
         System.out.println("1. Добавить");
@@ -46,7 +44,6 @@ public class MainApp {
         int option = in.nextInt();
         return option;
     }
-
     public static int displayModelType() {
         System.out.println("----------------Модель авто.----------------");
         System.out.println("1. Добавить");
@@ -57,7 +54,6 @@ public class MainApp {
         int option = in.nextInt();
         return option;
     }
-
     public static int displayBindings() {
         System.out.println("----------------Привязки------------------");
         System.out.println("1. Добавить");
@@ -69,6 +65,142 @@ public class MainApp {
         return option;
     }
 
+    private static void selectOptionToVehicle(int option) {
+        VehicleService vehicleService = new VehicleService();
+        VehicleConsoleInput vehicleInput = new VehicleConsoleInput();
+        VehicleConsoleOutput vehicleOutput = new VehicleConsoleOutput();
+        Vehicle vehicle;
+        long vehicleId;
+        switch (option) {
+            case 1:
+                vehicle = vehicleInput.input();
+                vehicleService.addVehicle(vehicle);
+                System.out.println("Добавлен");
+                break;
+            case 2:
+                vehicleId = vehicleInput.inputIdToRemove();
+                vehicleService.removeVehicle(vehicleId);
+                System.out.println("Удален");
+                break;
+            case 3:
+                vehicle = vehicleInput.input();
+                vehicleId = vehicleInput.inputIdToShow();
+                vehicleService.changeVehicle(vehicle, vehicleId);
+                System.out.println("Изменен");
+                break;
+            case 4:
+                vehicleId = vehicleInput.inputIdToShow();
+                vehicle = vehicleService.getVehicle(vehicleId);
+                vehicleOutput.print(vehicle);
+                break;
+            case 5:
+                List<Vehicle> vehicles = vehicleService.getAllVehicles();
+                vehicleOutput.printAll(vehicles);
+                break;
+        }
+    }
+    private static void selectOptionToDriver(int option) {
+        DriverService driverService = new DriverService();
+        DriverConsoleInput driverInput = new DriverConsoleInput();
+        Driver driver;
+        long driverId;
+        DriverConsoleOutput driverOutput = new DriverConsoleOutput();
+        switch (option) {
+            case 1:
+                driver = driverInput.input();
+                driverService.addDriver(driver);
+                System.out.println("Добавлен");
+                break;
+            case 2:
+                driverId = driverInput.inputIdToRemove();
+                driverService.removeDriver(driverId);
+                System.out.println("Удален");
+                break;
+            case 3:
+                driver = driverInput.input();
+                driverId = driverInput.inputIdToShow();
+                driverService.changeDriver(driver, driverId);
+                System.out.println("Изменен");
+                break;
+            case 4:
+                driverId = driverInput.inputIdToShow();
+                driver = driverService.getDriver(driverId);
+                driverOutput.print(driver);
+                break;
+            case 5:
+                List<Driver> drivers = driverService.getAllDrivers();
+                driverOutput.printAll(drivers);
+                break;
+        }
+    }
+    private static void selectOptionToModelType(int option) {
+        ModelTypeService modelService = new ModelTypeService();
+        ModelTypeInput modelInput = new ModelTypeInput();
+        ModelTypeOutput modelOuput = new ModelTypeOutput();
+        long modelId;
+        ModelType modelType;
+        switch (option) {
+            case 1:
+                modelType = modelInput.input();
+                modelService.addModelType(modelType);
+                System.out.println("Добавлен");
+                break;
+            case 2:
+                modelId = modelInput.inputIdToRemove();
+                modelService.removeModelType(modelId);
+                System.out.println("Удален");
+                break;
+            case 3:
+                modelType = modelInput.input();
+                modelId = modelInput.inputIdToShow();
+                modelService.changeModelType(modelType, modelId);
+                System.out.println("Изменен");
+                break;
+            case 4:
+                modelId = modelInput.inputIdToShow();
+                modelType = modelService.getModelType(modelId);
+                modelOuput.print(modelType);
+                break;
+            case 5:
+                List<ModelType> modelTypes = modelService.getAllTypes();
+                modelOuput.printAll(modelTypes);
+                break;
+        }
+    }
+    private static void selectOptionToDvm2m(int option) {
+        Dvm2mService dvm2mService = new Dvm2mService();
+        Dvm2mConsoleInput dvm2mInput = new Dvm2mConsoleInput();
+        Dvm2mConsoleOutput dvm2mOutput = new Dvm2mConsoleOutput();
+        long dvm2mId;
+        Dvm2m dvm2m;
+        switch (option){
+            case 1:
+                dvm2m = dvm2mInput.input();
+                dvm2mService.addDriverAndCarLink(dvm2m);
+                System.out.println("Добавлен");
+                break;
+            case 2:
+                dvm2mId = dvm2mInput.inputIdToRemove();
+                dvm2mService.removeDriverAndCarLink(dvm2mId);
+                System.out.println("Удален");
+                break;
+            case 3:
+                dvm2m = dvm2mInput.input();
+                dvm2mId = dvm2mInput.inputIdToShow();
+                dvm2mService.changeDriverAndCarLink(dvm2m, dvm2mId);
+                System.out.println("Изменен");
+                break;
+            case 4:
+                dvm2mId = dvm2mInput.inputIdToShow();
+                dvm2m = dvm2mService.getDriverAndCarLink(dvm2mId);
+                dvm2mOutput.print(dvm2m);
+                break;
+            case 5:
+                List<Dvm2m> dvm2ms = dvm2mService.getDriverAndCarLins();
+                dvm2mOutput.printAll(dvm2ms);
+                break;
+        }
+    }
 
     public static void main(String[] args) {
         int value = displayMainMenu();
@@ -76,143 +208,19 @@ public class MainApp {
         switch (value) {
             case 1:
                 option = displayCarsInfo();
-                VehicleService vehicleService = new VehicleService();
-                VehicleConsoleInput vehicleInput = new VehicleConsoleInput();
-                VehicleConsoleOutput vehicleOutput = new VehicleConsoleOutput();
-                Vehicle vehicle;
-                long vehicleId;
-                switch (option) {
-                    case 1:
-                        vehicle = vehicleInput.input();
-                        vehicleService.addVehicle(vehicle);
-                        System.out.println("Добавлен");
-                        break;
-                    case 2:
-                        vehicleId = vehicleInput.inputIdToRemove();
-                        vehicleService.removeVehicle(vehicleId);
-                        System.out.println("Удален");
-                        break;
-                    case 3:
-                        vehicle = vehicleInput.input();
-                        vehicleId = vehicleInput.inputIdToShow();
-                        vehicleService.changeVehicle(vehicle, vehicleId);
-                        System.out.println("Изменен");
-                        break;
-                    case 4:
-                        vehicleId = vehicleInput.inputIdToShow();
-                        vehicle = vehicleService.getVehicle(vehicleId);
-                        vehicleOutput.print(vehicle);
-                        break;
-                    case 5:
-                        List<Vehicle> vehicles = vehicleService.getAllVehicles();
-                        vehicleOutput.printAll(vehicles);
-                        break;
-                }
+                selectOptionToVehicle(option);
                 break;
             case 2:
                 option = displayDriversInfo();
-                DriverService driverService = new DriverService();
-                DriverConsoleInput driverInput = new DriverConsoleInput();
-                Driver driver;
-                long driverId;
-                DriverConsoleOutput driverOutput = new DriverConsoleOutput();
-                switch (option) {
-                    case 1:
-                        driver = driverInput.input();
-                        driverService.addDriver(driver);
-                        System.out.println("Добавлен");
-                        break;
-                    case 2:
-                        driverId = driverInput.inputIdToRemove();
-                        driverService.removeDriver(driverId);
-                        System.out.println("Удален");
-                        break;
-                    case 3:
-                        driver = driverInput.input();
-                        driverId = driverInput.inputIdToShow();
-                        driverService.changeDriver(driver, driverId);
-                        System.out.println("Изменен");
-                        break;
-                    case 4:
-                        driverId = driverInput.inputIdToShow();
-                        driver = driverService.getDriver(driverId);
-                        driverOutput.print(driver);
-                        break;
-                    case 5:
-                        List<Driver> drivers = driverService.getAllDrivers();
-                        driverOutput.printAll(drivers);
-                        break;
-                }
+                selectOptionToDriver(option);
                 break;
             case 3:
                 option = displayModelType();
-                ModelTypeService modelService = new ModelTypeService();
-                ModelTypeInput modelInput = new ModelTypeInput();
-                ModelTypeOutput modelOuput = new ModelTypeOutput();
-                long modelId;
-                ModelType modelType;
-                switch (option) {
-                    case 1:
-                        modelType = modelInput.input();
-                        modelService.addModelType(modelType);
-                        System.out.println("Добавлен");
-                        break;
-                    case 2:
-                        modelId = modelInput.inputIdToRemove();
-                        modelService.removeModelType(modelId);
-                        System.out.println("Удален");
-                        break;
-                    case 3:
-                        modelType = modelInput.input();
-                        modelId = modelInput.inputIdToShow();
-                        modelService.changeModelType(modelType, modelId);
-                        System.out.println("Изменен");
-                        break;
-                    case 4:
-                        modelId = modelInput.inputIdToShow();
-                        modelType = modelService.getModelType(modelId);
-                        modelOuput.print(modelType);
-                        break;
-                    case 5:
-                        List<ModelType> modelTypes = modelService.getAllTypes();
-                        modelOuput.printAll(modelTypes);
-                        break;
-                }
+                selectOptionToModelType(option);
                 break;
             case 4:
                 option = displayBindings();
-                Dvm2mService dvm2mService = new Dvm2mService();
-                Dvm2mConsoleInput dvm2mInput = new Dvm2mConsoleInput();
-                Dvm2mConsoleOutput dvm2mOutput = new Dvm2mConsoleOutput();
-                long dvm2mId;
-                Dvm2m dvm2m;
-                switch (option){
-                    case 1:
-                        dvm2m = dvm2mInput.input();
-                        dvm2mService.addDriverAndCarLink(dvm2m);
-                        System.out.println("Добавлен");
-                        break;
-                    case 2:
-                        dvm2mId = dvm2mInput.inputIdToRemove();
-                        dvm2mService.removeDriverAndCarLink(dvm2mId);
-                        System.out.println("Удален");
-                        break;
-                    case 3:
-                        dvm2m = dvm2mInput.input();
-                        dvm2mId = dvm2mInput.inputIdToShow();
-                        dvm2mService.changeDriverAndCarLink(dvm2m, dvm2mId);
-                        System.out.println("Изменен");
-                        break;
-                    case 4:
-                        dvm2mId = dvm2mInput.inputIdToShow();
-                        dvm2m = dvm2mService.getDriverAndCarLink(dvm2mId);
-                        dvm2mOutput.print(dvm2m);
-                        break;
-                    case 5:
-                        List<Dvm2m> dvm2ms = dvm2mService.getDriverAndCarLins();
-                        dvm2mOutput.printAll(dvm2ms);
-                        break;
-                }
+                selectOptionToDvm2m(option);
                 break;
             default:
                 System.exit(0);
